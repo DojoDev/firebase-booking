13f005d4e617a6d7e937e72309739cd3b7b4d295
DojoDev <dojodevbr@gmail.com>
2019-02-21 21:28:04 -0300

    Create a new page SingIn and singUp

---
+++ src/pages/singin/singin.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SinginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-singin',
+  templateUrl: 'singin.html',
+})
+export class SinginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+  singUp(){
+    this.navCtrl.push('SingupPage');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SinginPage');
+  }
+
+}
